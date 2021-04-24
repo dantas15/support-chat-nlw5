45538502be86f1f3e9452fb307c58294e11d8229
Gustavo Dantas Galote <gusgalote@outlook.com>
2021-04-24 00:41:13 -0300

    5th Class (Finishing the project)

--- src/http.ts
+++ src/http.ts
@@ -16,6 +16,9 @@ app.set("view engine", "html");
 app.get("/pages/client", (request, response) => {
   return response.render("html/client.html");
 });
+app.get("/pages/admin", (request, response) => {
+  return response.render("html/admin.html");
+});
 
 const http = createServer(app); // Creating http protocol
 const io = new Server(http); // Creating ws protocol
--- src/services/ConnectionsService.ts
+++ src/services/ConnectionsService.ts
@@ -35,5 +35,32 @@ class ConnectionsService {
 
     return connection;
   }
+
+  async findAllWithoutAdmin() {
+    const connections = await this.connectionsRepository.find({
+      where: { admin_id: null },
+      relations: ["user"],
+    });
+
+    return connections;
+  }
+
+  async findBySocketID(socket_id: string) {
+    const connection = await this.connectionsRepository.findOne({
+      socket_id,
+    });
+    return connection;
+  }
+
+  async updateAdminID(user_id: string, admin_id: string) {
+    await this.connectionsRepository
+      .createQueryBuilder()
+      .update(Connection)
+      .set({ admin_id })
+      .where("user_id = :user_id", {
+        user_id,
+      })
+      .execute();
+  }
 }
 export { ConnectionsService };
